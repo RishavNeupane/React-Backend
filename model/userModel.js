f0e92210f33f60cfa7491f326e659f1b7cc21b84
RishavNeupane <rishavneuane98@gmail.com>
2026-02-13 07:56:04 +0545

    React Backend Learning

---
+++ model/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const userSchema = mongoose.Schema({
+    firstName:{
+        type: String,
+        require: true
+    },
+
+    lastName:{
+        type: String,
+        require: true
+    },
+
+    password:{
+        type: String,
+        require: true
+    },
+    
+    email:{
+        type: String,
+        require: true
+    },
+
+    isAdmin:{
+        type:Boolean,
+        require: false
+    }
+})
+
+module.exports = mongoose.model("User",userSchema);
